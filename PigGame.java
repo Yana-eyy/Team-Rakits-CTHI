7a7446218480a2b67d435879d730e11f264ae7e6
Alyana Soriano <127061416+Yana-eyy@users.noreply.github.com>
2023-03-06 14:11:43 +0800

    Update PigGame.java

--- PigGame.java
+++ PigGame.java
@@ -58,7 +58,7 @@ public class PigGame {
             }
         
                if (repeat == 'N')
-                
+                System.out.println("Turn ended. You earned " + playerScores + " points");
                   break;                               
         }}
             while(playerTotal <= 100);
