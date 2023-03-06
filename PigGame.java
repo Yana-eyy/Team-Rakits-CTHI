e3bbdcfd3f9167075b5ebcb46271f2af9c5d85de
cameronux <127071300+cameronux@users.noreply.github.com>
2023-03-06 13:02:26 +0800

    Update PigGame.java

--- PigGame.java
+++ PigGame.java
@@ -2,11 +2,30 @@
  * @author Alyana Soriano
  */
 import java.util.*;
-public class PigGame {
-    static Scanner pog = new Scanner (System.in);
-    private static int a;
-    public static void main (String []args){
-        System.out.println("Hello! Welcome to the Pig Game!");
-    }
 
+public class Main {
+  public static void main(String[] args) {
+
+      int playerScores = 0;
+      int playerTotal = 0;
+      int computerScores = 0;
+      int computerTotal = 0;
+      int dice;
+      boolean gameOver = false;
+      boolean turnOver = false;
+      char repeat;
+      String input;   
+      Scanner keyboard  = new Scanner(System.in);
+       
+      Random rand = new Random();
+    
+    System.out.println("Hello! Wellcome to pig game!");
+
+      while (gameOver == false)
+      {    
+         do
+         {
+          dice = rand.nextInt(6) + 1;
+          System.out.println("You rolled: " + dice);
+  }
 }
