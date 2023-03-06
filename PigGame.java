0fbdee51a45f1e38a0b1e1d5ddec018c4b5d42b6
Alyana Soriano <127061416+Yana-eyy@users.noreply.github.com>
2023-03-06 12:14:06 +0800

    Update PigGame.java

--- PigGame.java
+++ PigGame.java
@@ -3,12 +3,13 @@
  */
 import java.util.*;
 public class PigGame {
-    
+    private static int a;
     public static void main (String []args){
      String Y;
      String N;
      String playerchoice = "";
      System.out.println(playerchoice.equalsIgnoreCase("Y"));
+     
 
     }
 
