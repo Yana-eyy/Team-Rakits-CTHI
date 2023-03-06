086ab7f5a08f56f69465fde4fd679746d3715f5a
Alyana Soriano <127061416+Yana-eyy@users.noreply.github.com>
2023-03-06 12:06:06 +0800

    Update PigGame.java

--- PigGame.java
+++ PigGame.java
@@ -5,14 +5,10 @@ import java.util.*;
 public class PigGame {
     
     public static void main (String []args){
-        int tab;
-        Scanner pog = new Scanner (System.in);
-        System.out.println("Insert ");
-        tab = pog.nextInt();
-        System.out.println(tab);
-System.out.println("hatdog");
-//aughguys  iniwan niya aq na
-
+     String Y;
+     String N;
+     String playerchoice;
+     System.out.println(playerchoice.equalsIgnoreCase("Y"));
 
     }
 
